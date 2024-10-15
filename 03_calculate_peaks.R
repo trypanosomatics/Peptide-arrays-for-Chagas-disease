62d0e42a4660d83ba8c48cf989eb01d1ed23d5ec
RamiroQuinteros <rquinteros@estudiantes.unsam.edu.ar>
2024-10-15 14:21:14 -0300

    calculate peaks function added

---
+++ 03_calculate_peaks.R
@@ -0,0 +1,64 @@
+#!/usr/bin/env Rscript
+
+# Default values
+main_folder <- "."
+testing <- TRUE
+sources <- c("AR", "BO", "BR", "CO", "MX", "US")
+min_amount_of_peptides_in_peak <- 2
+# sd_multiplier_for_cutoff determines cutoff as: mode + sd_multiplier_for_cutoff * sd
+
+#### CONFIG for running in terminal####
+args <- commandArgs(trailingOnly = TRUE)
+
+for (i in seq(1, length(args), by = 2)) {
+  if (args[i] == "--main_folder") {
+    main_folder <- args[i + 1]
+  }
+  if (args[i] == "--testing") {
+    testing <- as.logical(args[i + 1])
+  }
+  if (args[i] == "--sources") {
+    sources <- unlist(strsplit(args[i + 1], ","))
+  }
+  if (args[i] == "--min_amount_of_peptides_in_peak") {
+    min_amount_of_peptides_in_peak <- as.numeric(args[i + 1])
+  }
+}
+
+#### PATH CONFIG ####
+output_folder <- sprintf("%s/outputs/03_pools_antigenic_peaks", project_folder)
+
+if (!dir.exists(output_folder)) {
+  dir.create(output_folder, recursive = TRUE)
+}
+
+output_file <- sprintf("%s/pools_peaks_cutoff4SD_2pep.tsv", output_folder)
+
+profile_data_folder <- sprintf("%s/outputs/02_pools_smoothed_data", project_folder)
+
+if (!dir.exists(profile_data_folder)) {
+  dir.create(profile_data_folder, recursive = TRUE)
+}
+
+profile_data_suffix <- "_smoothed_signals.tsv"
+
+#### Aux function path ####
+functions_folder <- sprintf("%s/functions", main_folder)
+functions_file <- sprintf("%s/03_calculate_peaks_function.R", functions_folder)
+
+#### OTHER PARAMETERS (DO NOT CHANGE) ####  
+types <- c("PO", "NE") #Positive and negative
+
+#In this design, the length of the peptides is 16 residues and the overlap is 12, it cannot be changed
+# If you use another microarray design, you can change it
+sequence_length <- 16 
+sequence_overlap <- 12
+
+other_type_proportion_decimals <- 2
+combined_mean_signal_decimals <- 2
+
+
+#### CALL MAIN FUNCTION ####
+source(functions_file)
+
+determine_peaks(main_folder, testing, sources, min_amount_of_peptides_in_peak, sd_multiplier_for_cutoff)
--- functions/01_pools_normalize_data_function.R
+++ functions/01_pools_normalize_data_function.R
@@ -22,9 +22,15 @@ calculateMode <- function(x, decimals = 0) {
 normalize_and_process_data <- function(raw_data_folder, output_folder, sources) {
   #Get data
   output_initialized <- FALSE
+  
+  # INTERNAL CONFIG (DO NOT CHANGE)
   sources_to_normalize_as_negatives <- c("LE")
   sources_to_exclude_from_global_statistics <- c("LE")
   types <- c("PO", "NE")
+  
+  output_signal_decimals <- 2
+  output_statistics_mode_decimals <- 2
+  output_statistics_sd_decimals <- 2
   
   for (source_for in sources) {
     # source_for <- sources[1]
@@ -34,24 +40,28 @@ normalize_and_process_data <- function(raw_data_folder, output_folder, sources) 
       raw_data <- fread(raw_data_file, header = TRUE, sep = "\t", na.strings = NULL)
       
       #Check if I need to initialize the tables to normalize
-      if (!output_initialized) {
+      if (output_initialized == F) {
         full_raw_PO_data <- raw_data[, .(`Reporter Name`)]
         full_raw_NE_data <- raw_data[, .(`Reporter Name`)]
-        output_initialized <- TRUE
+        output_initialized <- T
       }
       
       #Add the data to the corresponding table
       if ((type_for == "NE") | (source_for %in% sources_to_normalize_as_negatives)) {
-        full_raw_NE_data <- merge(full_raw_NE_data, raw_data, by = "Reporter Name")
+        full_raw_NE_data <- merge(full_raw_NE_data,
+                                  raw_data,
+                                  by = "Reporter Name")
       } else {
-        full_raw_PO_data <- merge(full_raw_PO_data, raw_data, by = "Reporter Name")
+        full_raw_PO_data <- merge(full_raw_PO_data,
+                                  raw_data,
+                                  by = "Reporter Name")
       }
     }
   }
   
   #Normalize the positive data
   matrix_PO_aux <- normalize.quantiles(as.matrix(full_raw_PO_data[, -c("Reporter Name")]))
-  matrix_PO_aux <- round(matrix_PO_aux, digits = 2)
+  matrix_PO_aux <- round(matrix_PO_aux, digits = output_signal_decimals)
   normalized_PO_data <- as.data.table(matrix_PO_aux)
   colnames(normalized_PO_data) <- colnames(full_raw_PO_data[, -c("Reporter Name")])
   normalized_PO_data$`Reporter Name` <- full_raw_PO_data$`Reporter Name`
@@ -61,7 +71,7 @@ normalize_and_process_data <- function(raw_data_folder, output_folder, sources) 
   
   #Normalize the negative data
   matrix_NE_aux <- normalize.quantiles(as.matrix(full_raw_NE_data[, -c("Reporter Name")]))
-  matrix_NE_aux <- round(matrix_NE_aux, digits = 2)
+  matrix_NE_aux <- round(matrix_NE_aux, digits = output_signal_decimals)
   normalized_NE_data <- as.data.table(matrix_NE_aux)
   colnames(normalized_NE_data) <- colnames(full_raw_NE_data[, -c("Reporter Name")])
   normalized_NE_data$`Reporter Name` <- full_raw_NE_data$`Reporter Name`
@@ -70,10 +80,13 @@ normalize_and_process_data <- function(raw_data_folder, output_folder, sources) 
   gc()
   
   #Combine data
-  full_normalized_data <- merge(normalized_PO_data, normalized_NE_data, by = "Reporter Name")
+  full_normalized_data <- merge(normalized_PO_data,
+                                normalized_NE_data,
+                                by = "Reporter Name")
   rm(normalized_PO_data)
   rm(normalized_NE_data)
   gc()
+  
   
   colnames(full_normalized_data) <- gsub("rawData", "normalizedData", colnames(full_normalized_data))
   
@@ -81,29 +94,37 @@ normalize_and_process_data <- function(raw_data_folder, output_folder, sources) 
   cols_to_select <- setdiff(colnames(full_normalized_data), "Reporter Name")
   if (length(sources_to_exclude_from_global_statistics) > 0) {
     for (source_for in sources_to_exclude_from_global_statistics) {
+      # source_for <- sources_to_exclude_from_global_statistics[1]
       cols_to_select <- cols_to_select[!grepl(sprintf("%s_", source_for), cols_to_select)]
     }
   }
   
-  global_signals <- as.vector(as.matrix(full_normalized_data[, cols_to_select, with = FALSE]))
-  mode_aux <- calculateMode(global_signals, decimals = 2)
-  sd_aux <- round(sd(global_signals), 2)
+  global_signals <- as.vector(as.matrix(full_normalized_data[, cols_to_select, with = F]))
   
-  global_statistics <- data.table(mode = mode_aux, sd = sd_aux)
+  mode_aux <- calculateMode(global_signals, decimals = output_statistics_mode_decimals)
+  sd_aux <- round(sd(global_signals), output_statistics_sd_decimals)
+  
+  global_statistics <- data.table(mode = mode_aux,
+                                  sd = sd_aux)
+  rm(global_signals)
+  gc()
   
   #### SAVE DATA ####
   global_statistics_output_file <- sprintf("%s/global_statistics.tsv", output_folder)
-  write.table(global_statistics, file = global_statistics_output_file, col.names = TRUE, row.names = FALSE, sep = "\t", quote = TRUE)
+  write.table(global_statistics, file = global_statistics_output_file, col.names = T, row.names = F, sep = "\t", quote = T)
   
   for (source_for in sources) {
+    # source_for <- sources[1]
     for (type_for in types) {
+      # type_for <- types[1]    
       cols_to_select <- colnames(full_normalized_data)
       cols_to_select <- cols_to_select[grepl(sprintf("%s_%s", source_for, type_for), cols_to_select)]
       cols_to_select <- c("Reporter Name", cols_to_select)
       
-      normalized_data_for <- full_normalized_data[, cols_to_select, with = FALSE]
-      normalized_data_output_file <- sprintf("%s/%s_%s_processed.tsv", output_folder, source_for, type_for)
-      write.table(normalized_data_for, file = normalized_data_output_file, col.names = TRUE, row.names = FALSE, sep = "\t", quote = TRUE)
+      normalized_data_for <- full_normalized_data[, cols_to_select, with = F]
+      
+      normalized_data_output_file <- sprintf("%s/%s_%s%s", output_folder, source_for, type_for, output_suffix)
+      write.table(normalized_data_for, file = normalized_data_output_file, col.names = T, row.names = F, sep = "\t", quote = T) 
     }
   }
 }
