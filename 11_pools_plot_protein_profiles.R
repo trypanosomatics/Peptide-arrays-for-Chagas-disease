18234e54f93eb77c857ffd03b09e1e5d4d05e955
guadaromer <gromer@iib.unsam.edu.ar>
2024-12-02 14:29:04 -0300

    Add script 11 - protein profiles

---
+++ 11_pools_plot_protein_profiles.R
@@ -0,0 +1,80 @@
+#!/usr/bin/env Rscript
+
+# CONFIGURACIÓN DE PARÁMETROS
+args <- commandArgs(trailingOnly = TRUE)
+
+# Default values
+main_folder <- "."
+#main_folder <- "Documentos/Guada/Array/Peptide-arrays-for-Chagas-disease"
+testing <- TRUE
+sources <- c("AR", "BO", "BR", "CO", "MX", "US", "LE")
+profile_data_suffix <- "smoothed.tsv"
+protein <- NULL  
+sd_multiplier_for_cutoff <- 4
+only_proteins_above <- 0
+only_proteins_below <- 0
+
+# User values
+for (i in seq(1, length(args), by = 2)) {
+  if (args[i] == "--main_folder") {
+    main_folder <- args[i + 1]
+  }
+  if (args[i] == "--testing") {
+    testing <- as.logical(args[i + 1])
+  }
+  if (args[i] == "--sources") {
+    sources <- args[i + 1]
+  }
+  if (args[i] == "--profile_data_suffix") {
+    profile_data_suffix <- args[i + 1]
+  }
+  if (args[i] == "--protein") {
+    protein <- args[i + 1]
+  }
+  if (args[i] == "--sd_multiplier_for_cutoff") {
+    sd_multiplier_for_cutoff <- args[i + 1]
+  }
+}
+
+# Print values
+cat("Main folder:", main_folder, "\n")
+cat("Testing:", testing, "\n")
+cat("Sources:", paste(sources, collapse = ", "), "\n")
+cat("Profile data suffix:", profile_data_suffix, "\n")
+cat("Protein (optional):", protein, "\n")
+cat("SD multiplier for cutoff>", sd_multiplier_for_cutoff, "\n")
+
+####PATH CONFIG####
+if (testing == TRUE) {
+  # For testing
+  project_folder <- sprintf("%s/test_data", main_folder)
+  extra_files_folder <- sprintf("%s/extra_files", main_folder)
+} else {
+  # For running the actual data
+  project_folder <- sprintf("%s/chagastope_data", main_folder)
+  extra_files_folder <- sprintf("%s/extra_files", main_folder)
+}
+
+input_folder <- sprintf("%s/outputs/02_pools_smoothed_data", project_folder)
+design_file <- sprintf("%s/inputs/01_pools_array_design/Supplementary File S08 - Mapping of CHAGASTOPE-v1 data to T cruzi proteins.tsv", project_folder)
+output_folder <- sprintf("%s/outputs/11_plots", project_folder)
+
+if (!dir.exists(output_folder)) {
+  dir.create(output_folder, recursive = TRUE)
+}
+
+# IMPORTAR FUNCIONES
+functions_folder <- sprintf("%s/functions", main_folder)
+source(sprintf("%s/11_plot_protein_profiles_function.R", functions_folder))
+
+# LLAMADA A LA FUNCIÓN PRINCIPAL
+plot_proteins(project_folder = project_folder,
+              input_folder = input_folder,
+              design_file = design_file,
+              sources = sources,
+              profile_data_suffix = profile_data_suffix,
+              output_folder = output_folder,
+              protein = protein,
+              only_proteins_above = only_proteins_above,
+              only_proteins_below = only_proteins_below,
+              sd_multiplier_for_cutoff = sd_multiplier_for_cutoff)
